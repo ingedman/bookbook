d9f0f966f329d59f965935c725cd169a03c02313
islam fakhry <islam1000man@gmail.com>
2019-06-05 21:01:20 +0200

    modified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'bio' => $faker->paragraph,
         'avatar' => $faker->imageUrl(120,120),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$u44M9HhFuUE0iJhdEKeHRuW1903ulol1ZgvWduLOVQfs9MECzYxoC', // password
         'remember_token' => Str::random(10),
     ];
 });
